ac70e56d2101bac00a1d654502ce9f9b907bbb72
Alexey Aleshin <aa7@inbox.ru>
2023-09-10 00:41:55 +0300

    pictures added

--- react-vite/src/index.ts
+++ react-vite/src/index.ts
@@ -1,8 +1,9 @@
-import {idea} from '../public/idea.png'
-import {idea2} from './assets/idea.png'
+import idea from '/idea.png'
+import idea2 from './assets/idea.png'
 
 const images={
     img1: idea,
-    img2: idea2
+    img2: idea2,
+    img3: "/idea.png"
 }
 export default images;
