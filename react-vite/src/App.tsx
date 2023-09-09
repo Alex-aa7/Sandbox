7f3d47e5f7669e3aa7a66d26943b8b9c0e384606
Alexey Aleshin <aa7@inbox.ru>
2023-09-09 23:58:25 +0300

    next

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import images from './index.js'
+import images from '../src/index.js'
 
 function App() {
   const [count, setCount] = useState(0)
